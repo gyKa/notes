f3652b3f517966bf9af451a2c58a17b8dcdb2ece
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-07-01 15:11:58 +0300

    Add ordering by ID for notes.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,7 +6,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $notes = app('db')->select("SELECT name, text FROM notes");
+        $notes = app('db')->select("SELECT name, text FROM notes ORDER BY id DESC");
 
         return view('index', ['notes' => $notes]);
     }
